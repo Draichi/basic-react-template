08e62cad964300e6310395bc29c754f5dd4a799b
draichi <lucasdraichi@gmail.com>
2021-03-20 23:46:31 -0300

    feat: adds menu item

--- src/App.js
+++ src/App.js
@@ -2,26 +2,25 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import FavoritesPage from "./pages/Favorites";
 import NewMeetupPage from "./pages/NewMeetup";
-import { Paper } from "@material-ui/core";
+import { Toolbar } from "@material-ui/core";
 
 import MainNavigation from "./layout/MainNavigation";
 function App() {
   return (
     <div>
       <MainNavigation />
-      <Paper>
-        <Switch>
-          <Route path="/" exact>
-            <AllMeetupsPage />
-          </Route>
-          <Route path="/new-meetup">
-            <NewMeetupPage />
-          </Route>
-          <Route path="/favorites">
-            <FavoritesPage />
-          </Route>
-        </Switch>
-      </Paper>
+      <Toolbar />
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/layout/MainNavigation.js
+++ src/layout/MainNavigation.js
@@ -1,19 +1,29 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar, Typography, Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
+
+import classes from "./MainNavigation.module.css";
 
 const MainNavigation = () => {
   return (
-    <AppBar position="static">
+    <AppBar>
       <Toolbar>
-        <Typography variant="h6">Meetups</Typography>
-        <Typography>
-          <Link to="/">All meetups</Link>
+        <Typography variant="h6" className={classes.title}>
+          Meetups
         </Typography>
         <Typography>
-          <Link to="/favorites">Favorites</Link>
+          <Button component={Link} to={"/"} color="inherit">
+            All meetups
+          </Button>
         </Typography>
         <Typography>
-          <Link to="/new-meetup">New meetup</Link>
+          <Button component={Link} to={"/favorites"} color="inherit">
+            Favorites
+          </Button>
+        </Typography>
+        <Typography>
+          <Button component={Link} to={"/new-meetup"} color="inherit">
+            New meetup
+          </Button>
         </Typography>
       </Toolbar>
     </AppBar>
